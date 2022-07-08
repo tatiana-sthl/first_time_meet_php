b09b5c00234b73764b5cfe605f6b200f55e5166d
Tatiana Stahli <stahli.tatiana@gmail.com>
2022-07-08 18:22:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# First experiments with php
+https://openclassrooms.com/fr/courses/918836-concevez-votre-site-web-avec-php-et-mysql
---
+++ numbers.php
@@ -0,0 +1,10 @@
+<?php
+$number1 = 2 + 4;
+echo 'The result of the number 1 is ' . $number1 . ' + the result of number 2 ';
+
+$number2 = 5 - 1;
+echo '(who is ' . $number2 . ') = ';
+
+$result = $number1 + $number2;
+echo $result;
+?>

79e6197acb4bce4f48059c21bac0ff3f43d32c11
Tatiana Stahli <stahli.tatiana@gmail.com>
2022-07-08 20:07:19 +0200

    add test about conditions

---
+++ conditions.php
@@ -0,0 +1,37 @@
+<?php
+$grade = 10;
+
+switch ($grade)
+{
+    case 0:
+        echo 'Nul';
+    break;
+
+    case 5:
+        echo 'Mauvais';
+    break;
+
+    case 7:
+        echo 'Toujours pas ouf';
+    break;
+
+    case 10:
+        echo 'Mouais';
+    break;
+
+    case 12:
+        echo 'Ok';
+    break;
+
+    case 15:
+        echo 'Bien';
+    break;
+
+    case 20:
+        echo 'Parfait';
+    break;
+
+    default:
+        echo "Note non reconnue";
+}
+?>
